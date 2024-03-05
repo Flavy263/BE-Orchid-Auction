64c43e089e1e10338dca6cda7f90325722febf1a
NhatLink <116991698+NhatLink@users.noreply.github.com>
2024-03-05 15:38:17 +0700

    Update AuctionController

--- routes/auction.js
+++ routes/auction.js
@@ -7,7 +7,26 @@ const authenticateJWT = passport.authenticate("jwt", { session: false });
 
 router.get("/", authenticateJWT, auctionController.getAllAuction);
 router.post("/", authenticateJWT, auctionController.createAuction);
-router.post("/user/:userID", authenticateJWT, auctionController.createAuction);
+router.post(
+  "/not-yet/:userID",
+  authenticateJWT,
+  auctionController.getAuctionNotYetAuctionedByUser
+);
+router.post(
+  "/about-to/:userID",
+  authenticateJWT,
+  auctionController.getAuctionAboutToAuctionByUser
+);
+router.post(
+  "/auctioning/:userID",
+  authenticateJWT,
+  auctionController.getAuctionAuctioningByUser
+);
+router.post(
+  "/autioned/:userID",
+  authenticateJWT,
+  auctionController.getAuctionaAuctionedByUser
+);
 router.get("/:auctionId", authenticateJWT, auctionController.getAuctionByID);
 router.put("/:auctionId", authenticateJWT, auctionController.updateAuctionByID);
 router.delete(
