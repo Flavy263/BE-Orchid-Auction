5d12bfd7bc9d2fd226429c79bae257cc9b2e69ba
Phat07 <nguyenhoangphat852@gmail.com>
2024-03-07 00:39:02 +0700

    fix bug code

--- routes/auction.js
+++ routes/auction.js
@@ -2,10 +2,6 @@ const express = require("express");
 const passport = require("passport");
 const auctionController = require("../controllers/auctionController");
 const auctionMemberController = require("../controllers/auctionMemberController");
-const express = require("express");
-const passport = require("passport");
-const auctionController = require("../controllers/auctionController");
-const auctionMemberController = require("../controllers/auctionMemberController");
 
 const router = express.Router();
 const authenticateJWT = passport.authenticate("jwt", { session: false });
