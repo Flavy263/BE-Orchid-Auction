aee87e131c8ff9be0a5d8021e542f4a4438d240e
nguynphmnaman@gmail.com <nguynphmnaman@gmail.com>
2024-03-06 16:20:47 +0700

    new api auction

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const roleRouter = require("./routes/role");
 const productRouter = require("./routes/product");
-// const auctionRouter = require("./routes/auction");
+const auctionRouter = require("./routes/auction");
 const walletRouter = require("./routes/wallet");
 const walletRequestRouter = require("./routes/walletRequest");
 const cors = require('cors');
@@ -70,7 +70,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/roles", roleRouter);
 app.use("/products", productRouter);
-// app.use("/auctions", auctionRouter);
+app.use("/auctions", auctionRouter);
 app.use("/wallets", walletRouter);
 app.use("/walletRequest", walletRequestRouter)
 // catch 404 and forward to error handler
--- routes/auction.js
+++ routes/auction.js
@@ -1,44 +1,64 @@
-// const express = require("express");
-// const passport = require("passport");
-// const auctionController = require("../controllers/auctionController");
-// const auctionMemberController = require("../controllers/auctionMemberController");
+const express = require("express");
+const passport = require("passport");
+const auctionController = require("../controllers/auctionController");
+const auctionMemberController = require("../controllers/auctionMemberController");
 
-// const router = express.Router();
-// const authenticateJWT = passport.authenticate("jwt", { session: false });
+const router = express.Router();
+const authenticateJWT = passport.authenticate("jwt", { session: false });
 
-// router.get("/", authenticateJWT, auctionController.getAllAuction);
-// router.post("/", authenticateJWT, auctionController.createAuction);
-// router.get(
-//   "/not-yet/:host_id",
-//   authenticateJWT,
-//   auctionController.getAuctionNotYetAuctionedByUser
-// );
-// router.get(
-//   "/about-to/:host_id",
-//   authenticateJWT,
-//   auctionController.getAuctionAboutToAuctionByUser
-// );
-// router.get(
-//   "/auctioning/:host_id",
-//   authenticateJWT,
-//   auctionController.getAuctionAuctioningByUser
-// );
-// router.get(
-//   "/autioned/:host_id",
-//   authenticateJWT,
-//   auctionController.getAuctionaAuctionedByUser
-// );
-// router.get("/:auctionId", authenticateJWT, auctionController.getAuctionByID);
-// router.put("/:auctionId", authenticateJWT, auctionController.updateAuctionByID);
-// router.delete(
-//   "/:auctionId",
-//   authenticateJWT,
-//   auctionController.deleteAuctionByID
-// );
-// router.post(
-//   "/saveMember",
-//   authenticateJWT,
-//   auctionMemberController.saveAuctionMember
-// );
+router.get("/", authenticateJWT, auctionController.getAllAuction);
+router.post("/", authenticateJWT, auctionController.createAuction);
+router.get(
+  "/not-yet",
+  authenticateJWT,
+  auctionController.getAuctionNotYetAuctioned
+);
+router.get(
+  "/about-to",
+  authenticateJWT,
+  auctionController.getAuctionAboutToAuction
+);
+router.get(
+  "/auctioning",
+  authenticateJWT,
+  auctionController.getAuctionAuctioning
+);
+router.get(
+  "/autioned",
+  authenticateJWT,
+  auctionController.getAuctionaAuctioned
+);
+router.get(
+  "/not-yet/:host_id",
+  authenticateJWT,
+  auctionController.getAuctionNotYetAuctionedByUser
+);
+router.get(
+  "/about-to/:host_id",
+  authenticateJWT,
+  auctionController.getAuctionAboutToAuctionByUser
+);
+router.get(
+  "/auctioning/:host_id",
+  authenticateJWT,
+  auctionController.getAuctionAuctioningByUser
+);
+router.get(
+  "/autioned/:host_id",
+  authenticateJWT,
+  auctionController.getAuctionaAuctionedByUser
+);
+router.get("/:auctionId", authenticateJWT, auctionController.getAuctionByID);
+router.put("/:auctionId", authenticateJWT, auctionController.updateAuctionByID);
+router.delete(
+  "/:auctionId",
+  authenticateJWT,
+  auctionController.deleteAuctionByID
+);
+router.post(
+  "/saveMember",
+  authenticateJWT,
+  auctionMemberController.saveAuctionMember
+);
 
-// module.exports = router;
+module.exports = router;
