e58daaf52b84b9a48ddec7c83ac6922a873cd638
Phat07 <nguyenhoangphat852@gmail.com>
2024-03-22 21:26:59 +0700

    fix

--- routes/auctionBid.js
+++ routes/auctionBid.js
@@ -7,9 +7,9 @@ const router = express.Router();
 // Define endpoint to handle new bid
 router.post('/:auctionId/:customerId/new-bid', auctionBidController.handleNewBid);
 
-router.get('/auctionBid/:auctionId',auctionBidController.handleFindMaxPrice)
-router.get('/auctionBid-sortDes/:auctionId',auctionBidController.getAuctionBidSortDes)
-router.get('/getMemberDoNotBid/:auctionId',auctionBidController.getMemberDoNotBid)
-router.get('/getAuctionHaveMemberDoNotBid',auctionBidController.getAuctionHaveMemberDoNotBid)
-router.get('/getUnregisteredAuction',auctionBidController.getUnregisteredAuction)
+router.get('/auctionBid/:auctionId', auctionBidController.handleFindMaxPrice)
+router.get('/auctionBid-sortDes/:auctionId', auctionBidController.getAuctionBidSortDes)
+router.get('/getMemberDoNotBid/:auctionId', auctionBidController.getMemberDoNotBid)
+router.get('/getAuctionHaveMemberDoNotBid/:host_id', auctionBidController.getAuctionHaveMemberDoNotBid)
+router.get('/getUnregisteredAuction/:host_id', auctionBidController.getUnregisteredAuction)
 module.exports = router;
